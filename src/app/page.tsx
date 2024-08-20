224a6595380f98ccbedbab45eee9cc696cd45303
Richard H. <richard@haddads.net.au>
2024-08-20 23:16:17 +1000

    not-found page draft done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import HomeHero from "@/components/home-hero-section";
 import QRCreator from "@/components/qr-creator";
 import HomeInfo from "@/components/home-info-section";
+import MainNavigation from "@/components/main-navigation";
+import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="mb-8 flex min-h-screen flex-col items-center justify-between">
+      <MainNavigation />
+
       <HomeHero />
 
       <QRCreator />
