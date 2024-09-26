bbd2b34c2c1a27b25bf40b0eeab14a28e85cba20
Richard H. <richard@haddads.net.au>
2024-09-26 21:27:11 +1000

    avoid using external server for svg

--- src/utils/qr-save.ts
+++ src/utils/qr-save.ts
@@ -1,41 +1,14 @@
 ﻿import { cleanTitle } from "@/utils/general";
 
-const outlineSVG = async (svgElement: SVGElement) => {
-  const clonedSVG = svgElement.cloneNode(true) as SVGElement;
-  const svgString = new XMLSerializer().serializeToString(clonedSVG);
-
-  try {
-    const response = await fetch(`https://170.64.215.144:3000/outline-svg`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        svg: svgString,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch outline SVG");
-    }
-
-    const { outlinedSVG } = await response.json();
-    return outlinedSVG;
-  } catch (error) {
-    console.error("Error outlineSVG:", error);
-    return svgString;
-  }
-};
-
-export const downloadToSVG = async (svgData: null | Node, title: string) => {
+export const downloadToSVG = (svgData: null | Node, title: string) => {
   if (!svgData) return;
 
-  const cloneData = svgData.cloneNode(true) as SVGElement;
-  cloneData.querySelector("path")!.setAttribute("stroke-width", "1");
+  const clonedSvg = svgData.cloneNode(true) as SVGElement;
+  clonedSvg.querySelector("path")?.setAttribute("stroke-width", "1");
 
   // Serialize the SVG element to get the SVG source code
   const serializer = new XMLSerializer();
-  const svgSource = serializer.serializeToString(cloneData);
+  const svgSource = serializer.serializeToString(clonedSvg);
 
   const svgBlob = new Blob([svgSource], {
     type: "image/svg+xml;charset=utf-8",
