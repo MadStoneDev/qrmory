09dd915ecdc5b2bf59d82f75383a98ffea8ff8e8
Richard H. <richard@haddads.net.au>
2025-03-04 00:53:05 +1000

    tidy up login page

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -1,5 +1,4 @@
 import { LoginBlock } from "@/components/login-block";
-import { createClient } from "@/utils/supabase/server";
 
 export const metadata = {
   title: "Login to your account | QRmory",
@@ -7,16 +6,6 @@ export const metadata = {
     "Access your arsenal of QR Codes to create, manage, and customize dynamic QR codes with ease.",
 };
 
-export async function fetchUser() {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  return user;
-}
-
 export default async function SignUpPage() {
-  console.log(await fetchUser());
-
   return <LoginBlock />;
 }
