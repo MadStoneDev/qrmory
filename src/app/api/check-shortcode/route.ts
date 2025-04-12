a754a09e176612a4aa8022833d489de49213d8a5
Richard H. <richard@haddads.net.au>
2025-04-13 00:16:51 +1000

    fix my codes

--- src/app/api/check-shortcode/route.ts
+++ src/app/api/check-shortcode/route.ts
@@ -1,6 +1,4 @@
-﻿"use server";
-
-import { NextRequest, NextResponse } from "next/server";
+﻿import { NextRequest, NextResponse } from "next/server";
 import { Redis } from "@upstash/redis";
 import { createClient } from "@/utils/supabase/server";
 
