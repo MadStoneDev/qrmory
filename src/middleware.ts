a759f846431edf006a96c121d509c49905a72083
Richard H. <richard@haddads.net.au>
2025-04-13 15:31:48 +1000

    fix middleware for public pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,51 @@
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
+
+import { createClient } from "@/utils/supabase/client";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const path = request.nextUrl.pathname;
+
+  // Public Routes
+  const publicRoutes = [
+    "/",
+    "/login",
+    "/register",
+    "/help",
+    "/privacy-policy",
+    "/terms-and-conditions",
+    "/cookies-policy",
+    "/error",
+  ];
+
+  // Skip for static files and API routes
+  if (
+    path.startsWith("/_next") ||
+    path.startsWith("/api") ||
+    path.includes(".")
+  ) {
+    return NextResponse.next();
+  }
+
+  // Skip for public routes
+  if (publicRoutes.includes(path)) {
+    return NextResponse.next();
+  }
+
+  // Check if user is authenticated
+  try {
+    const supabase = createClient();
+    const { data } = await supabase.auth.getUser();
+
+    if (!data) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return await updateSession(request);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.redirect(new URL("/error", request.url));
+  }
 }
 
 export const config = {
